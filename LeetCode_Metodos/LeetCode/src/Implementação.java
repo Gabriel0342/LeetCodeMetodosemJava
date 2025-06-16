336adece9c4afe99d23f97d75543a84783a2edbe
Gabriel0342 <163338928+Gabriel0342@users.noreply.github.com>
2025-06-16 11:28:35 +0100

    update

--- LeetCode_Metodos/LeetCode/src/Implementação.java
+++ LeetCode_Metodos/LeetCode/src/Implementação.java
@@ -1,6 +1,6 @@
 public class Implementação {
     public static void main(String[] args) {
-        int []n = {3,0,1};
-        System.out.println(Metodos.findGreatestCommonDivisor(n));
+        int n = 28;
+        System.out.println(Metodos.checkPerfectNumber(n));
     }
 }
